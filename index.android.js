cc90f1f494def6b3f8ea4c807d80fca66a91ef88
angelossdias <D14sAng>
2018-07-25 17:27:12 -0300

    Usando state

--- index.android.js
+++ index.android.js
@@ -9,25 +9,40 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  Button,
   View
 } from 'react-native';
 
 class MeuComponente extends Component {
+
   render() {
     return (
       <View>
-        <Text>{this.props.propriedade1}</Text>
-        <Text>{this.props.xyz}</Text>
-        <Text>{this.props.outraProp}</Text>
+        <Text>{this.props.teste}</Text>
       </View>
     );
   }
+
 }
 
 class pedraPapelTesoura extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {texto: 'Texto teste 2'};
+  }
+
+  alteraTexto() {
+    this.setState({texto: 'Outra coisa '});
+  }
+
   render() {
     return (
-      <MeuComponente propriedade1='Banana' xyz='Abacaxi' outraProp='Uva'></MeuComponente>
+      <View>
+        <MeuComponente teste={this.state.texto}/>
+        <Button title='Botão' onPress={() => {this.alteraTexto()}} />
+      </View>
     );
   }
 }
